b69d004ee5de51649976ae049f3b27ea878e45e0
William Dinauer <william.p.dinauer.23@dartmouth.edu>
2020-05-22 13:21:58 -0400

    entry updates

--- Pikr/app/src/main/java/com/example/pikr/fragments/EntryFragment.java
+++ Pikr/app/src/main/java/com/example/pikr/fragments/EntryFragment.java
@@ -2,11 +2,15 @@ package com.example.pikr.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.pikr.R;
 
@@ -16,11 +20,12 @@ import com.example.pikr.R;
  * create an instance of this fragment.
  */
 public class EntryFragment extends Fragment {
+    private TextView mTitle, mDescription;
+    private Button mPostButton, mPhotoButton;
     public EntryFragment() {
         // Required empty public constructor
     }
 
-    // TODO: Rename and change types and number of parameters
     public static EntryFragment newInstance() {
         return new EntryFragment();
     }
@@ -30,5 +35,15 @@ public class EntryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_entry, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mTitle = view.findViewById(R.id.title_text);
+        mDescription = view.findViewById(R.id.description_text);
+        mPhotoButton = view.findViewById(R.id.add_photo_button);
+        mPostButton = view.findViewById(R.id.save_post_button);
+
     }
 }
